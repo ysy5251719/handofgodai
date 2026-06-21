aca6259cce42a79290ff9896f02b29365435b697
ysy5251719 <4rfmcjk4t2@privaterelay.appleid.com>
2026-06-21 20:44:42 +0800

    Create LiveComments.tsx

---
+++ src/components/LiveComments.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const LiveComments: React.FC = () => {
+  return (
+    <div className="absolute bottom-4 left-4 right-4 bg-white/90 rounded-lg p-4 shadow-lg">
+      <h3 className="font-bold mb-2">实时评论</h3>
+      <div className="text-sm text-gray-600">评论功能开发中...</div>
+    </div>
+  );
+};
